9848272535d2c8b9eb220bbebd51f836121cad1c
rksvw <ritik.vw98@gmail.com>
2025-07-08 22:35:16 +0530

    added keydown event listener

--- src/Component/DrumPadComponent.js
+++ src/Component/DrumPadComponent.js
@@ -7,11 +7,14 @@ export default function DrumPadComponent() {
   const [audioTitle, setAudioTitle] = useState("");
   const [volume, setVolume] = useState(0.5);
   const [isPowerOn, setIsPowerOn] = useState(true);
+  const [displayText, setDisplayText] = useState("");
 
   return (
     <div className="d_machine" id="drum-machine">
-      <div className="drum_keys">
-        <DrumPadKeys setAudioTitle={setAudioTitle} volume={volume} isPowerOn={isPowerOn} setIsPowerOn={setIsPowerOn} />
+      <div className="drum_keys" id="display">
+        {displayText}
+        <DrumPadKeys setAudioTitle={setAudioTitle} volume={volume} isPowerOn={isPowerOn} setIsPowerOn={setIsPowerOn}
+        setDisplayText={setDisplayText} />
       </div>
       <div className="drum_set">
         <DrumPadSet
--- src/Component/DrumPadKeys.js
+++ src/Component/DrumPadKeys.js
@@ -1,10 +1,11 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { SampleMusics } from "../Service/DrumPadKeyService";
 import "./DrumPadKeys.css";
 
-function DrumPadKeys({ setAudioTitle, volume, isPowerOn }) {
+function DrumPadKeys({ setAudioTitle, volume, isPowerOn, setDisplayText }) {
   const audioRefs = useRef({});
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const playAudio = (key) => {
     const _audio_ref = audioRefs.current[key];
 
@@ -12,9 +13,25 @@ function DrumPadKeys({ setAudioTitle, volume, isPowerOn }) {
       setAudioTitle(_audio_ref.current.title);
       _audio_ref.current.currentTime = 0;
       _audio_ref.current.volume = volume; // Set volume here (0.0 to 1.0)
+      setDisplayText(key ?? "");
       _audio_ref.current.play();
     }
   };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      const key = e.key.toUpperCase();
+      if (SampleMusics.some((item) => item.key === key)) {
+        playAudio(key);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [playAudio]);
 
   return (
     <div className="drum-keys">
@@ -26,10 +43,12 @@ function DrumPadKeys({ setAudioTitle, volume, isPowerOn }) {
         return (
           <div
             key={item.key}
-            className="cols"
+            className="cols drum-pad"
             onClick={() => playAudio(item.key)}
             id={item.name}
+            onKeyDown={() => playAudio(item.key)}
           >
+            {item.key}
             <audio
               src={item.source}
               id={item.key}
@@ -37,7 +56,6 @@ function DrumPadKeys({ setAudioTitle, volume, isPowerOn }) {
               title={item.name}
               className="clip"
             ></audio>
-            {item.key}
           </div>
         );
       })}
